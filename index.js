d74a330de4c9992a8bd149f1391e615c4dbecfde
ArmanIslamTC <armanislam.ais@gmail.com>
2025-11-09 19:20:28 +0800

    added mongodb, course posting

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const admin = require("firebase-admin");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xbf1ip3.mongodb.net/?appName=Cluster0
+`;// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      const db = client.db('learn-hub')
+      const usersCollection = db.collection('users')
+      const courseCollection = db.collection('courses')
+
+      ///users
+      app.post("/users", async (req, res) => {
+        const newUser = req.body;
+        const email = req.body.email;
+        const query = { email: email };
+        const existingUser = await usersCollection.findOne(query);
+        if (existingUser) {
+          res.send({ message: "user exits" });
+        } else {
+          const result = await usersCollection.insertOne(newUser);
+          res.send(result);
+        }
+      });
+
+      /// course posting
+      app.post('/create-course', async (req, res) => {
+          const newCourse = req.body
+          const result = await courseCollection.insertOne(newCourse)
+          res.send(result)
+      })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("server running");
+});
+
+app.listen(port, () => {
+  console.log("server running on:", port);
+});
